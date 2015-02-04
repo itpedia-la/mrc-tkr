12074dbcd43e404423d89fe684551bb836e488d8
Somwang Souksavatd <dsouksavatd@gmail.com>
2015-02-04 09:11:35 +0700

    update

--- app/views/seat/index.blade.php
+++ app/views/seat/index.blade.php
@@ -4,12 +4,14 @@
 <div align="center">
 <form id="frmSeat">
 <!--  <input type="text" class="k-textbox"> <input type="text" class="k-textbox"> -->
+
 ການສະແດງວັນທີ: <select id="showDate">
 		<option value="2015-02-22" @if(Route::input('showDate')=="2015-02-22") selected="selected" @endif>22-02-2015</option>
 		<option value="2015-02-23" @if(Route::input('showDate')=="2015-02-23") selected="selected" @endif>23-02-2015</option>
 	</select>
 <button class="k-button k-primary" type="button" id="btnSeatOk">ຕົກລົງ</button>  <button class="k-button" id="btnReset">ເລີ່ມໃຫມ່</button>
 <p>ກະລຸນາເລືອກບ່ອນນັ່ງ</p>
+
 </div>
 <hr />
 	<!-- Begine: VIP ZONE -->
@@ -93,6 +95,7 @@
 
 		var content = '<div><form id="frmSubmit"><input type="hidden" value="'+seatData+'" name="seat"><input type="hidden" value="'+showDate+'" name="showDate">';
 				content+= '<table>';
+				@if( Auth::id() )
 				content+= '<tr><td colspan="2" align="center"><span class="tag green" id="message">ກະລຸນາໃສ່ຂໍ້ມູນຕິດຕໍ່</span></td></tr>';
 				content+= '<tr><td colspan="2" align="center"><hr/></tr>';
 					content+= '<tr><td width="40%" align="right">ຊື່ ແລະ ນາມສະກຸນ: *</td><td><input type="text" class="k-textbox" id="fullname" name="fullname"></td></tr>';
@@ -103,6 +106,12 @@
 					@endif
 					content+= '<tr><td colspan="2" align="center"><hr/></tr>';
 					content+= '<tr><td width="40%" align="right">&nbsp;</td><td><button type="submit" class="k-button k-primary">ຕົກລົງ</button> <button type="button" class="k-button" id="btnCancel">ຍົກເລີກ</button></td></tr>';
+				@else
+					content+= '<tr><td colspan="2" align="center"><p style="margin:50px 0px 0px 0px">ຕິດຕໍ່ ທີ່ເບີ 020 7897 7711, 020 7897 7722 ແລະ 020 7897 7733 ຢືນຢັນການຈອງແລະຊຳລະເງິນ</p></td></tr>';
+					content+= '<tr><td colspan="2" align="center"><p style="margin:50px 0px 0px 0px">ຂໍຂອບໃຈ,</p></td></tr>';
+					content+= '<tr><td colspan="2" align="center"><hr/></tr>';
+					content+= '<tr><td width="40%" align="right">&nbsp;</td><td><button type="submit" class="k-button k-primary" id="btnCancel">ຕົກລົງ</button></td></tr>';
+				@endif
 				content+= '</table>';
 			content+= '</form></div>';
 
